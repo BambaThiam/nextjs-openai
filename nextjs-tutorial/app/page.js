4814dbf35207677715255ad4104036489bc9676f
DayOneAI <bambathiam88@gmail.com>
2024-02-25 23:26:53 +0100

    feat: ✨Pages - Link Component - Nested Routes #2

--- nextjs-tutorial/app/page.js
+++ nextjs-tutorial/app/page.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl font-bold'>HomePage</h1>
-      <Link href="/about" className="text-2xl">About Page </Link>
+      <Link href="/about" className="text-2xl">About Page - </Link>
       <Link href="/contact" className="text-2xl">Contact Page</Link>
     </div>
   )
