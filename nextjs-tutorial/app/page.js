e3e95743d93fb530b3e3d4b09a5a0a286dd5ec98
DayOneAI <bambathiam88@gmail.com>
2024-02-25 23:23:16 +0100

    Create pages - Link Component - Nested Routes #2

--- nextjs-tutorial/app/page.js
+++ nextjs-tutorial/app/page.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import Link from "next/link"
 
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl font-bold'>HomePage</h1>
+      <Link href="/about" className="text-2xl">About Page </Link>
+      <Link href="/contact" className="text-2xl">Contact Page</Link>
     </div>
   )
 }
