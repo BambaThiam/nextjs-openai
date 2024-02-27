e7335358668df97cb8cfe5c528e3f727628b6d69
DayOneAI <bambathiam88@gmail.com>
2024-02-27 23:27:06 +0100

    feat: ✨Challenge - Fetch Data in Drinks Page #8

--- nextjs-tutorial/app/client/page.js
+++ nextjs-tutorial/app/client/page.js
@@ -5,9 +5,9 @@ const ClientPage = () => {
   const [count, setCount] = useState(0)
   return (
     <div className='text-7xl flex gap-4 m-4'>
-      <button onClick={() => setCount((prevCount) => prevCount - 1)} className='btn'>-</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)} className='btn btn-primary'>Decrease</button>
       <button className='btn'>{count}</button>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)} className='btn'>+</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)} className='btn btn-primary'>Increase</button>
     </div>
   )
 }
--- nextjs-tutorial/app/drinks/page.js
+++ nextjs-tutorial/app/drinks/page.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const DrinksPage = () => {
-  return (
-    <div className='text-7xl'>DrinksPage</div>
-  )
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
+// const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=ab'
+
+const fetchDrinks = async () => {
+    // just for demo purposes
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(URL);
+    if (!response.ok) {
+        throw new Error('Failed to fetch drinks...');
+    }
+    const data = await response.json();
+    return data;
+}
+
+
+const DrinksPage = async () => {
+   const data = await fetchDrinks()
+   console.log(data)
+    return (
+      <div className='text-7xl'>DrinksPage</div>
+    )
 }
 
 export default DrinksPage
