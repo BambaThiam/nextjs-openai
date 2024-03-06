a47344e91b767494457145326027f6a5a8700e52
DayOneAI <bambathiam88@gmail.com>
2024-03-07 00:08:25 +0100

    feat: ✨Pages creation #36

---
+++ gptgenius/app/(dashboard)/layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div>
+        <div className=''>
+            <h1>Layout</h1>
+        </div>
+        {children}
+    </div>
+  )
+}
+
+export default layout
