c49e79c24203d2fd3d47188ae3721edcb4012ca9
DayOneAI <bambathiam88@gmail.com>
2024-03-08 00:57:53 +0100

    feat: ✨Sidebar stup #39

--- gptgenius/app/sign-in/[[...sign-in]]/page.js
+++ gptgenius/app/sign-in/[[...sign-in]]/page.js
@@ -2,8 +2,7 @@ import { SignIn } from "@clerk/nextjs";
  
 const SignInPage = () => {
     return (
-    //   <div className='min-h-screen flex justify-center items-center'>
-      <div className=''>
+      <div className='min-h-screen flex justify-center items-center'>
         <SignIn />
       </div>
     );
--- gptgenius/app/sign-up/[[...sign-up]]/page.js
+++ gptgenius/app/sign-up/[[...sign-up]]/page.js
@@ -2,8 +2,7 @@ import { SignUp } from "@clerk/nextjs";
  
 const SignInPage = () => {
     return (
-    //   <div className='min-h-screen flex justify-center items-center'>
-      <div className=''>
+      <div className='min-h-screen flex justify-center items-center'>
         <SignUp />
       </div>
     );
