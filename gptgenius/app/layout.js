59f6040e8e4df6b825a8e5a1886c10af0bedd087
DayOneAI <bambathiam88@gmail.com>
2024-03-09 11:21:56 +0100

    feat: ✨Add React-Hot-Toast Library #43

--- gptgenius/app/layout.js
+++ gptgenius/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>{children}</body>
+        <Providers>{children}</Providers>
       </html>
     </ClerkProvider>
     
---
+++ gptgenius/app/providers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+
+const Providers = ({children}) => {
+  return (
+    <div>
+        <Toaster position='top-center' />
+        {children}
+    </div>
+  )
+}
+
+export default Providers
