e2f8680d54a838183cb64200243274fea13cfecd
DayOneAI <bambathiam88@gmail.com>
2024-03-16 14:45:00 +0100

    feat: ✨search functionality #50

--- gptgenius/app/(dashboard)/tours/page.js
+++ gptgenius/app/(dashboard)/tours/page.js
@@ -4,16 +4,16 @@ import { getAllTours } from "@/utils/action"
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query"
 
 const AllToursPage = async () => {
-  const queryClient = new QueryClient()
-  await queryClient.prefetchQuery({ 
-    queryKey: ['tours'],
-    queryFn: () => getAllTours
-  })
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['tours', ""],
+    queryFn: () => getAllTours(),
+  });
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <ToursPage/>
+      <ToursPage />
     </HydrationBoundary>
-  )
+  );
 }
 
 export default AllToursPage
